0927a5b7b06abd4d9aefe9d97e8441e7194f04ce
Johan Dahl <johan.dahl.92@hotmail.com>
2021-06-22 19:32:30 +0200

    adds MacOS notifications and console logs

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Berlin Bürgeramt appointment finder
 
-This tool finds the available appointment dates for the Berlin Bürgeramt. This script does not book appointments, it only finds them and logs them to a file. This script was used to measure the availability of appointments for an Anmeldung in Berlin.
+This tool finds the available appointment dates for the Berlin Bürgeramt. This script does not book appointments, it only finds them and logs them to a file and pushes a notification to the Mac OS Notification Center. 
+
+The requests will return 419 after some time...
 
 If you need an appointment as soon as possible, [there are better ways](http://allaboutberlin.com/guides/berlin-burgeramt-appointment).
--- appointments.py
+++ appointments.py
@@ -1,17 +1,23 @@
 from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
+import pync
 import requests
 import json
 import logging
 import time
 
+TITLE = 'Bürgeramt Appointment Finder'
+BASE_URL = 'https://service.berlin.de/terminvereinbarung/termin/'
 
 ALL_BUERGERAMTS = (
-    122210, 122217, 122219, 122227, 122231, 122243, 122252, 122260, 122262,
-    122254, 122273, 122277, 122280, 122282, 122284, 122291, 122285, 122286,
-    122296, 327262, 325657, 150230, 122301, 122297, 122294, 122312, 122314,
-    122304, 122311, 122309, 317869, 324434, 122281, 324414, 122283, 122279,
-    122276, 122274, 122267, 122246, 122251, 122257, 122208, 122226
+    122210,122217,327316,122219,327312,122227,327314,122231,327346,122243,
+    327348,122252,329742,122260,329745,122262,329748,122254,329751,122271,
+    327278,122273,327274,122277,327276,122280,327294,122282,327290,122284,
+    327292,327539,122291,327270,122285,327266,122286,327264,122296,327268,
+    150230,329760,122301,327282,122297,327286,122294,327284,122312,329763,
+    122304,327330,122311,327334,122309,327332,122281,327352,122279,329772,
+    122276,327324,122274,327326,122267,329766,122246,327318,122251,327320,
+    122257,327322,122208,327298,122226,327300
 )
 
 ANMELDUNG_SERVICE_ID = 120686
@@ -34,7 +40,7 @@ def get_appointment_dates(buergeramt_ids=ALL_BUERGERAMTS, service_id=ANMELDUNG_S
         'dienstleisterlist': ','.join(buergeramt_ids),
         'anliegen[]': service_id,
     }
-    response = requests.get('https://service.berlin.de/terminvereinbarung/termin/tag.php', params=params, headers=headers)
+    response = requests.get(BASE_URL + 'tag.php', params=params, headers=headers)
     soup = BeautifulSoup(response.text, 'html.parser')
 
     month_widgets = soup.find_all(class_='calendar-month-table')
@@ -48,13 +54,17 @@ def get_appointment_dates(buergeramt_ids=ALL_BUERGERAMTS, service_id=ANMELDUNG_S
         available_day_links = month_widget.find_all('td', class_='buchbar')
         available_days = [int(link.find('a').text) for link in available_day_links]
         available_dates += [today.replace(month=displayed_month, day=available_day) for available_day in available_days]
-
+    if (response.status_code == 200):
+        print('Checked on %s and found %i appointments' % (datetime.now().strftime('%Y-%m-%dT%H:%M:%S'), len(available_day_links)))
+    else:
+        print('%s - Status code' % response.status_code)
     return available_dates
 
 
 def log_appointment_dates(dates):
     """
-    Writes the appointment dates in a file. Each line is written as a JSON object.
+    Logs results in a file. Each line is written as a JSON object.
+    Notifies the user if a date is found.
     """
     logging.basicConfig(filename='dates.log', format='%(message)s', level=logging.INFO)
     date_strings = [d.strftime('%Y-%m-%dT%H:%M:%S') for d in dates]
@@ -62,6 +72,8 @@ def log_appointment_dates(dates):
     	'timestamp': datetime.now().strftime('%Y-%m-%dT%H:%M:%S'),
     	'available_dates': date_strings
     }))
+    if (len(dates)):
+        pync.notify('Found %s available time(s)' % len(date_strings), title=TITLE, open=BASE_URL+'day/')
 
 
 def observe(limit, polling_delay):
@@ -79,4 +91,5 @@ def observe(limit, polling_delay):
 
 
 if __name__ == "__main__":
+    pync.notify('Started looking for available times...', title=TITLE)
     observe(timedelta(days=30), polling_delay=60)
